287d09405da37633425b15bd519ae6c16b046e99
wizjin <wizjin@icloud.com>
2018-12-21 18:49:04 +0800

    Remove unused header files.

--- libxps/libxps.h
+++ libxps/libxps.h
@@ -9,10 +9,24 @@
 #ifndef __LIBXPS_H__
 #define __LIBXPS_H__
 
-#if defined(__cplusplus)
-#   define LIBXPS_EXPORT   extern "C"
+#ifdef __OBJC__
+#ifdef TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#elif TARGET_OS_MAC
+#import <Cocoa/Cocoa.h>
 #else
-#   define LIBXPS_EXPORT   extern
+#import <Foundation/Foundation.h>
+#endif
+#endif
+
+#ifdef FOUNDATION_EXPORT
+#   define LIBXPS_EXPORT    FOUNDATION_EXPORT
+#else
+#if defined(__cplusplus)
+#   define LIBXPS_EXPORT    extern "C"
+#else
+#   define LIBXPS_EXPORT    extern
+#endif
 #endif
 
 LIBXPS_EXPORT const char *xps_version(void);
